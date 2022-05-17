22faf3633ae01ebe74b2f24875a92aa1bcb6ee3a
FourTis Four <fourtisgaming@gmail.com>
2022-05-17 17:10:33 +0300

    Profile View

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:i_sucialize/profile.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomeView(),
+      home: ProfileView(),
     );
   }
 }
