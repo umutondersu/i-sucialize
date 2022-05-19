a49bc48c0b66d65e4a2bfbc21df05f4dba2034c9
nisadefneAKSU <92059217+nisadefneAKSU@users.noreply.github.com>
2022-05-19 21:38:34 +0300

    Add files via upload

---
+++ lib/util/colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color.fromRGBO(0, 72, 144, 1);
+  //Dark Blue
+  static const Color backgroundcolor2 = Color.fromRGBO(64, 118, 172, 1);
+  //Light Blue
+  static const Color textcolor = Colors.white;
+  static const Color textcolor2 = Colors.white70;
+  static const Color secondary = Color.fromARGB(255, 78, 116, 241);
+  //Light BLue again.. should be cooler
+  static const Color backgroundcolor = Colors.black;
+  static const Color n_primary = Colors.lightBlue;
+  static const Color n_secondary = Color(0xFF80D8FF);
+
+  static const Color textColor = Color(0xFF424242);
+  static const Color headingColor = Color(0xFF0277BD);
+  static const Color captionColor = Colors.white30;
+  static const Color darkButtonTextColor = Colors.white;
+  static const Color appBarTitleTextColor = Colors.white;
+  static const Color textFieldFillColor = Color(0xFFD0D0D0);
+  static const Color mainbackgroundcolor = Color.fromRGBO(25, 25, 25, 1);
+
+}
+
