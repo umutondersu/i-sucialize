39c175c83d70d0b822c4ee70c38f6bafa56b0e3b
CodedTheBear <berkinan@sabanciuniv.edu>
2022-05-19 14:36:28 +0300

    appbar

---
+++ lib/AppBar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  PageController pageController = PageController();
+
+  void onTapped (int index){
+    setState((){
+      _selectedIndex = index;
+    });
+    pageController.animateToPage(index, duration: Duration(milliseconds: 500), curve: Curves.easeIn);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+        BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add'),
+        BottomNavigationBarItem(icon: Icon(Icons.chat_rounded), label: 'Chat'),
+        BottomNavigationBarItem(icon: Icon(Icons.notifications), label: 'Notifs')
+
+      ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          onTap: onTapped
+      ),
+    );
+  }
+}
