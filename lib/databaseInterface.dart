05169ab6b911551e652a7197b88e2d13f1ebfd75
FourTis Four <fourtisgaming@gmail.com>
2022-06-12 20:28:59 +0300

    Post

--- lib/databaseInterface.dart
+++ lib/databaseInterface.dart
@@ -1,9 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseInterface {
-
   final String uid;
-  DatabaseInterface({required this.uid });
+  DatabaseInterface({required this.uid});
 
   /*Future<void> updateUserData(String sugars, String name, int strength) async {
     return await _userCollection.doc(uid).set({
@@ -13,23 +12,40 @@ class DatabaseInterface {
     });
   }*/
 
-  Stream<DocumentSnapshot> getUserStream(){
+  Future<void> updateUserData(Map<String, dynamic> data) async {
+    return await FirebaseFirestore.instance
+        .collection('users')
+        .doc(uid)
+        .update(data);
+  }
+
+  Stream<DocumentSnapshot> getUserStream() {
     return FirebaseFirestore.instance.collection('users').doc(uid).snapshots();
   }
 
-  Stream<DocumentSnapshot> getPostStream({required String postID}){
-    return FirebaseFirestore.instance.collection('posts').doc(postID).snapshots();
+  Future<DocumentSnapshot<Map<String, dynamic>>> getUser() {
+    return FirebaseFirestore.instance.collection('users').doc(uid).get();
   }
 
-  Stream<QuerySnapshot> getAllPostsStream(){
+  Stream<DocumentSnapshot> getPostStream({required String postID}) {
+    return FirebaseFirestore.instance
+        .collection('posts')
+        .doc(postID)
+        .snapshots();
+  }
+
+  Stream<QuerySnapshot> getAllPostsStream() {
     return FirebaseFirestore.instance.collection('posts').snapshots();
   }
 
-  Stream<DocumentSnapshot> getMessageStream({required String messageID}){
-    return FirebaseFirestore.instance.collection('Messages').doc(messageID).snapshots();
+  Stream<DocumentSnapshot> getMessageStream({required String messageID}) {
+    return FirebaseFirestore.instance
+        .collection('Messages')
+        .doc(messageID)
+        .snapshots();
   }
 
-  Stream<QuerySnapshot> getAllMessagesStream(){
+  Stream<QuerySnapshot> getAllMessagesStream() {
     return FirebaseFirestore.instance.collection('Messages').snapshots();
   }
 }
--- lib/post.dart
+++ lib/post.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:i_sucialize/databaseInterface.dart';
 import 'package:i_sucialize/profile.dart';
 import 'package:path/path.dart';
 import 'package:i_sucialize/home.dart';
@@ -36,7 +37,7 @@ class PostViewState extends State<PostView> {
     });
   }
 
-  Future post() async {
+  void post(String username, int count) async {
     String post = postController.text;
 
     await uploadImageToFirebase();
@@ -44,17 +45,15 @@ class PostViewState extends State<PostView> {
     int votes = 0;
 
     final data = {
-      //'username': prof.username,
+      'username': username,
       'post': post,
       'image': mediaUrl,
       'date': date,
       'votes': votes
     };
 
-    postController.clear();
-
     var db = FirebaseFirestore.instance;
-    db
+    await db
         .collection("posts")
         .add(data)
         .then((value) => {
@@ -69,9 +68,12 @@ class PostViewState extends State<PostView> {
                 error = true;
               })
             });
+    if (!error) {
+      postController.clear();
+      int c = count + 1;
+      databaseInterface!.updateUserData({'postCount': c});
+    }
   }
-
-  Future postData() async {}
 
   StorageService _storageService = StorageService();
 
@@ -93,120 +95,131 @@ class PostViewState extends State<PostView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('New Post'),
-          centerTitle: true,
-          elevation: 0,
-          foregroundColor: AppColors.textcolor,
-          backgroundColor: AppColors.primary,
-          leading: Padding(
-            padding: EdgeInsets.all(10),
-            child: FlatButton(
-              padding: EdgeInsets.all(0),
-              onPressed: () {
-                Navigator.pushNamed(context, '/profile');
-              },
-              child: CircleAvatar(
-                child: ClipOval(
-                  child: Image.network(
-                    "https://static.wikia.nocookie.net/amogus/images/c/cb/Susremaster.png/revision/latest/scale-to-width-down/1200?cb=20210806124552",
-                    fit: BoxFit.cover,
+    return StreamBuilder<DocumentSnapshot>(
+        stream: databaseInterface!.getUserStream(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
+          return Scaffold(
+              appBar: AppBar(
+                title: const Text('New Post'),
+                centerTitle: true,
+                elevation: 0,
+                foregroundColor: AppColors.textcolor,
+                backgroundColor: AppColors.primary,
+                leading: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: FlatButton(
+                    padding: EdgeInsets.all(0),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/profile');
+                    },
+                    child: CircleAvatar(
+                      child: ClipOval(
+                        child: Image.network(
+                          snapshot.data!['image'],
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      backgroundColor: AppColors.primary,
+                      radius: 100,
+                    ),
                   ),
                 ),
-                backgroundColor: AppColors.primary,
-                radius: 100,
+                leadingWidth: 80,
               ),
-            ),
-          ),
-          leadingWidth: 80,
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                          color: AppColors.backgroundcolor2, width: 1),
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      color: AppColors.backgroundcolor2,
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          _image != null
-                              ? Image.file(File(_image!.path))
-                              : Image.asset('lib/assets/images/1.png'),
-                          Padding(
-                            padding: EdgeInsets.all(10),
+              body: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                color: AppColors.backgroundcolor2, width: 1),
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            color: AppColors.backgroundcolor2,
                           ),
-                          Text(
-                            "Your Post:",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          TextField(
-                            controller: postController,
-                            decoration: InputDecoration(
-                              counter: null,
-                              hintStyle: TextStyle(color: Colors.white),
-                              border: InputBorder.none,
+                          child: Padding(
+                            padding: EdgeInsets.all(20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                _image != null
+                                    ? Image.file(File(_image!.path))
+                                    : Image.asset('lib/assets/images/1.png'),
+                                Padding(
+                                  padding: EdgeInsets.all(10),
+                                ),
+                                Text(
+                                  "Your Post:",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                TextField(
+                                  controller: postController,
+                                  decoration: InputDecoration(
+                                    counter: null,
+                                    hintStyle: TextStyle(color: Colors.white),
+                                    border: InputBorder.none,
+                                  ),
+                                  style: TextStyle(
+                                      color: AppColors.textcolor, fontSize: 20),
+                                ),
+                              ],
                             ),
-                            style: TextStyle(
-                                color: AppColors.textcolor, fontSize: 20),
                           ),
-                        ],
-                      ),
+                          width: MediaQuery.of(context).size.width * 0.85,
+                          height: MediaQuery.of(context).size.height * 0.66,
+                        ),
+                      ],
                     ),
-                    width: MediaQuery.of(context).size.width * 0.85,
-                    height: MediaQuery.of(context).size.height * 0.66,
-                  ),
-                ],
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Color.fromRGBO(0, 0, 0, 0), width: 1),
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            color: Color.fromRGBO(0, 72, 144, 1),
+                          ),
+                          width: 130,
+                          height: 50,
+                          child: Center(
+                            child: TextButton(
+                              child: Text("Post",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 25)),
+                              onPressed: () {
+                                post(snapshot.data!['username'],
+                                    snapshot.data!['postCount']);
+                              },
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                          child: IconButton(
+                            onPressed: pickImage,
+                            icon: Icon(Icons.upload),
+                            iconSize: 50,
+                            color: Colors.white,
+                            splashRadius: 30,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                          color: Color.fromRGBO(0, 0, 0, 0), width: 1),
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      color: Color.fromRGBO(0, 72, 144, 1),
-                    ),
-                    width: 130,
-                    height: 50,
-                    child: Center(
-                      child: TextButton(
-                        child: Text("Post",
-                            style:
-                                TextStyle(color: Colors.white, fontSize: 25)),
-                        onPressed: post,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                    child: IconButton(
-                      onPressed: pickImage,
-                      icon: Icon(Icons.upload),
-                      iconSize: 50,
-                      color: Colors.white,
-                      splashRadius: 30,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-        backgroundColor: Color.fromRGBO(25, 25, 25, 1));
+              backgroundColor: Color.fromRGBO(25, 25, 25, 1));
+        });
   }
 }
