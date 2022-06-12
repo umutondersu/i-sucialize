d59ae768f56a54821fe6ecba1f092615886f8b70
Umut Önder <umutsonder@gmail.com>
2022-06-12 19:16:14 +0300

    added user query

--- lib/databaseInterface.dart
+++ lib/databaseInterface.dart
@@ -1,9 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseInterface {
-
   final String uid;
-  DatabaseInterface({required this.uid });
+  DatabaseInterface({required this.uid});
 
   /*Future<void> updateUserData(String sugars, String name, int strength) async {
     return await _userCollection.doc(uid).set({
@@ -13,24 +12,34 @@ class DatabaseInterface {
     });
   }*/
 
-  Stream<DocumentSnapshot> getUserStream(){
+  Stream<DocumentSnapshot> getUserStream() {
     return FirebaseFirestore.instance.collection('users').doc(uid).snapshots();
   }
 
-  Stream<DocumentSnapshot> getPostStream({required String postID}){
-    return FirebaseFirestore.instance.collection('posts').doc(postID).snapshots();
+  Stream<DocumentSnapshot> getPostStream({required String postID}) {
+    return FirebaseFirestore.instance
+        .collection('posts')
+        .doc(postID)
+        .snapshots();
   }
 
-  Stream<QuerySnapshot> getAllPostsStream(){
+  Stream<QuerySnapshot> getAllPostsStream() {
     return FirebaseFirestore.instance.collection('posts').snapshots();
   }
 
-  Stream<DocumentSnapshot> getMessageStream({required String messageID}){
-    return FirebaseFirestore.instance.collection('Messages').doc(messageID).snapshots();
+  Stream<DocumentSnapshot> getMessageStream({required String messageID}) {
+    return FirebaseFirestore.instance
+        .collection('Messages')
+        .doc(messageID)
+        .snapshots();
   }
 
-  Stream<QuerySnapshot> getAllMessagesStream(){
+  Stream<QuerySnapshot> getAllMessagesStream() {
     return FirebaseFirestore.instance.collection('Messages').snapshots();
+  }
+
+  Stream<QuerySnapshot> getAllUsersStream() {
+    return FirebaseFirestore.instance.collection('users').snapshots();
   }
 }
 
